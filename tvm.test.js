9e69375350f3f63f3e4bc63eb23f92c60b6ab362
Zhanna Klimanova <zklimanova@yahoo.com>
2024-05-05 12:53:56 -0400

    Adding findPercentage conversion function

--- tvm.test.js
+++ tvm.test.js
@@ -3,7 +3,7 @@ import { findPercentage } from './utils'
 import TVM from './tvm'
 
 test('Tests compoundAmount function with rate 0.1 and 2 periods', () => {
-  expect(TVM.F.P(0.1, 2)).toBeCloseTo(1.21);
+  expect(TVM.F.P(findPercentage(10), 2)).toBeCloseTo(1.21);
 });
 
 test('Tests presentValueAmount function with rate 0.1 and 2 periods', () => {
