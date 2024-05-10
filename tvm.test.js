1bb584b9e15926c62320e1d8f174163d59cde17d
Nafiz Islam <nafiz.islam1001@gmail.com>
2024-05-10 08:51:27 -0400

    wip

--- tvm.test.js
+++ tvm.test.js
@@ -12,7 +12,7 @@ describe('Testing Simple Interest Rate', () => {
         { n: 2, i: 100, expected: -3 },
         { n: Infinity, i: 50, expected: -Infinity },
     ])('n=$n, i=$i, P_simple($i, $n)=$expected', ({ n, i, expected }) => {
-        expect(TVM.F.P_simple(findPercentage(i), n)).toBeCloseTo(expected);
+        expect(TVM.F.P_simple(findPercentage(i), n)).toBeCloseTo(expected, 5);
     })
 })
 
@@ -27,9 +27,10 @@ describe('Testing TVM Formulas: (F/P,i,n), (P/F,i,n), (F/A,i,n), (A/F,i,n), (P/A
             { n: 1, i: 100, value: -2 },
             { n: 2, i: 50, value: -2.25 },
             { n: 2, i: 100, value: -4 },
+            { n: 13, i: 23, value: -14.74913 },
         ])('(F/P,$i,$n)=1/(P/F,$i,$n)=$value', (({ n, i, value }) => {
-            expect(TVM.F.P(findPercentage(i), n)).toBeCloseTo(value);
-            expect(1 / TVM.P.F(findPercentage(i), n)).toBeCloseTo(value);
+            expect(TVM.F.P(findPercentage(i), n)).toBeCloseTo(value, 5);
+            expect(1 / TVM.P.F(findPercentage(i), n)).toBeCloseTo(value, 5);
         }))
         test('(F/P,0,Infinity)=1/(P/F,0,Infinity)=NaN', () => {
             expect(TVM.F.P(0, Infinity)).toBe(NaN);
@@ -59,9 +60,11 @@ describe('Testing TVM Formulas: (F/P,i,n), (P/F,i,n), (F/A,i,n), (A/F,i,n), (P/A
             { n: 1, i: 100, value: -0.5 },
             { n: 2, i: 50, value: -10 / 9 },
             { n: 2, i: 100, value: -0.75 },
+            { n: 13, i: 23, value: -4.05304 },
+            { n: Infinity, i: 23, value: -1/findPercentage(23) },
         ])('(P/A,$i,$n)=1/(A/P,$i,$n)=$value', (({ n, i, value }) => {
-            expect(TVM.P.A(findPercentage(i), n)).toBeCloseTo(value);
-            expect(1 / TVM.A.P(findPercentage(i), n)).toBeCloseTo(value);
+            expect(TVM.P.A(findPercentage(i), n)).toBeCloseTo(value, 5);
+            expect(1 / TVM.A.P(findPercentage(i), n)).toBeCloseTo(value, 5);
         }))
     })
 
@@ -74,10 +77,18 @@ describe('Testing TVM Formulas: (F/P,i,n), (P/F,i,n), (F/A,i,n), (A/F,i,n), (P/A
             { n: 1, i: 100, value: -1 },
             { n: 2, i: 50, value: -2.5 },
             { n: 2, i: 100, value: -3 },
+            { n: 13, i: 23, value: -59.77883 },
         ])('(F/A,$i,$n)=1/(A/F,$i,$n)=$value', (({ n, i, value }) => {
-            expect(TVM.F.A(findPercentage(i), n)).toBeCloseTo(value);
-            expect(1 / TVM.A.F(findPercentage(i), n)).toBeCloseTo(value);
+            expect(TVM.F.A(findPercentage(i), n)).toBeCloseTo(value, 5);
+            expect(1 / TVM.A.F(findPercentage(i), n)).toBeCloseTo(value, 5);
         }))
+
+        test('(F/A,50,Infinity)=Infinity', () => {
+            expect(TVM.F.A(findPercentage(50), Infinity)).toBe(-Infinity)
+        })
+        test('(A/F,50,Infinity)=0', () => {
+            expect(TVM.A.F(findPercentage(50), Infinity)).toBe(-0)
+        })
     })
 
     describe('Testing (A/G,i,n)', () => {
@@ -89,11 +100,13 @@ describe('Testing TVM Formulas: (F/P,i,n), (P/F,i,n), (F/A,i,n), (A/F,i,n), (P/A
             { n: 2, i: 100, value: -0.33333333333333337 },
             { n: 3, i: 50, value: -0.736842105263158 },
             { n: 3, i: 100, value: -0.5714285714285714 },
+            { n: 13,i: 23, value: -3.4023118217153705}
         ])('(A/G,$i,$n)=$value', (({ n, i, value }) => {
-            expect(TVM.A.G(findPercentage(i), n)).toBeCloseTo(value);
+            expect(TVM.A.G(findPercentage(i), n)).toBeCloseTo(value, 5);
         }))
     })
 
+    // FIXME!!!
     describe('Testing (P/C,i,g,n)', () => {
         test.each([
             { n: 1, i: 50, g: 0, value: 0 },
@@ -102,8 +115,9 @@ describe('Testing TVM Formulas: (F/P,i,n), (P/F,i,n), (F/A,i,n), (A/F,i,n), (P/A
             { n: 2, i: 100, g: 0, value: -0.33333333333333337 },
             { n: 3, i: 50, g: 0, value: -0.736842105263158 },
             { n: 3, i: 100, g: 0, value: -0.5714285714285714 },
-        ])('(P/C,$i,$g,$n)=$value', (({ n, i, value }) => {
-            expect(TVM.A.G(findPercentage(i), n)).toBeCloseTo(value);
+            { n: 13,i: 23, g: 0, value: -3.4023118217153705}
+        ])('(P/C,$i,$g,$n)=$value', (({ n, i, g, value }) => {
+            expect(TVM.P.C(findPercentage(i), g, n)).toBeCloseTo(value, 4);
         }))
     })
 })
