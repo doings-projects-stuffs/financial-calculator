dbf7c80db63deb0f777afadbb7458b3ee987d652
Nafiz Islam <nafiz.islam1001@gmail.com>
2024-05-10 18:13:36 -0400

    finish Testing (P/C,i,g,n)

--- tvm.test.js
+++ tvm.test.js
@@ -109,16 +109,24 @@ describe('Testing TVM Formulas: (F/P,i,n), (P/F,i,n), (F/A,i,n), (A/F,i,n), (P/A
     // FIXME!!!
     describe('Testing (P/C,i,g,n)', () => {
         test.each([
-            { n: 1, i: 50, g: 0, value: 0 },
-            { n: 1, i: 100, g: 0, value: 0 },
-            { n: 2, i: 50, g: 0, value: -0.3999999999999999 },
-            { n: 2, i: 100, g: 0, value: -0.33333333333333337 },
-            { n: 3, i: 50, g: 0, value: -0.736842105263158 },
-            { n: 3, i: 100, g: 0, value: -0.5714285714285714 },
-            { n: 13,i: 23, g: 0, value: -3.4023118217153705}
+            { n: 1, i: 50, g: 0, value: -0.6666666666666667 },
+            { n: 1, i: 100, g: 10, value: -0.49999999999999994 },
+            { n: 2, i: 50, g: 20, value: -1.2000000000000004 },
+            { n: 2, i: 100, g: 30, value: -0.825 },
+            { n: 3, i: 50, g: 40, value: -1.8696296296296326 },
+            { n: 3, i: 100, g: 50, value: -1.15625 },
+            { n: 13,i: 23, g: 60, value: -79.82345049344173}
         ])('(P/C,$i,$g,$n)=$value', (({ n, i, g, value }) => {
-            expect(TVM.P.C(findPercentage(i), g, n)).toBeCloseTo(value, 4);
+            expect(TVM.P.C(findPercentage(i), findPercentage(g), n)).toBeCloseTo(value, 5);
         }))
+
+        test('(P/C,50%,25%,Infinity)=-1/(50% - 25%)', () => {
+            expect(TVM.P.C(0.5, 0.25, Infinity)).toBeCloseTo(-1 / (0.5 - 0.25));
+        })
+
+        test('(P/C,50%,50%,13)=-13/(1 + 50%)', () => {
+            expect(TVM.P.C(0.5, 0.5, 13)).toBeCloseTo(-13/(1 + 0.5));
+        })
     })
 })
 
