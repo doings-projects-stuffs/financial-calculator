45d8340caf8566c3e66e9081e828afca9bd79060
Nafiz Islam <nafiz.islam1001@gmail.com>
2024-05-14 21:48:28 -0400

    refactor main.js a little

--- main.js
+++ main.js
@@ -1,21 +1,20 @@
 import { TVM } from './src/tvm.js';
 import { findPercentage } from './src/utils.js';
 
-function createListeningObject(id) {
-    let variable = { value: '' }
-    document.getElementById(id).addEventListener('input', (event) => {
-        variable.value = event.target.value;
-        console.log(`${id}: ${variable.value}`);
-    })
-
-    return variable;
+const state = {
+    PV: '',
+    FV: '',
+    PMT: '',
+    IY: '',
+    N: ''
 }
 
-const pv = createListeningObject('PV')
-const fv = createListeningObject('FV')
-const pmt = createListeningObject('PMT')
-const iy = createListeningObject('IY')
-const n = createListeningObject('N')
+for (const key in state) {
+    document.getElementById(key).addEventListener('input', (event) => {
+        state[key] = event.target.value;
+        console.log(`${key}: ${state[key]}`);
+    })
+}
 
 function calculateTVM() {
     // const pv = parseFloat(document.getElementById('PV').value);
@@ -25,13 +24,7 @@ function calculateTVM() {
     // const n = parseFloat(document.getElementById('N').value);
     // const FV = pv * TVM.F.P(findPercentage(iy), n);
     // document.getElementById('tvm-result').textContent = `Future Value: ${FV.toFixed(2)}`;
-    console.info({
-        pv: pv.value,
-        fv,
-        pmt: pmt.value,
-        iy,
-        n
-    })
+    console.info(state)
 }
 
 const button = document.getElementById("tvm-calculate")
