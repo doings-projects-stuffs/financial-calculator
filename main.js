e97141cd7cfe249437bbed176bea3c675f80f024
Nafiz Islam <nafiz.islam1001@gmail.com>
2024-05-24 07:58:59 -0400

    take advantage of state variable

--- main.js
+++ main.js
@@ -17,14 +17,8 @@ for (const key in state) {
 }
 
 function calculateTVM() {
-    const pv = parseFloat(document.getElementById('PV').value);
-    const pmt = parseFloat(document.getElementById('PMT').value);
-    const fv = parseFloat(document.getElementById('FV').value);
-    const iy = parseFloat(document.getElementById('IY').value);
-    const n = parseFloat(document.getElementById('N').value);
-    const FV = pv * TVM.F.P(findPercentage(iy), n);
+    const FV = +state.PV * TVM.F.P(findPercentage(+state.IY), +state.N);
     document.getElementById('tvm-result').textContent = `Future Value: ${FV.toFixed(2)}`;
-    console.info(state)
 }
 
 const button = document.getElementById("tvm-calculate")
