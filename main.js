58db776a0fdaffe16095bf37b6ec9b67f5968016
Nafiz Islam <nafiz.islam1001@gmail.com>
2024-06-20 19:31:37 -0400

    hopefully final commit?

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import { TVM } from './src/tvm.js';
-import { findPercentage } from './src/utils.js';
+import { TVM } from "./src/tvm.js";
+import { findPercentage } from "./src/utils.js";
 
 const tvmVariables = {
     PV: {
@@ -21,20 +21,19 @@ const tvmVariables = {
     N: {
         description: "Periods",
         value: "",
-    }
-}
+    },
+};
 
-const tvmResult = document.getElementById('tvm-result')
+const tvmResult = document.getElementById("tvm-result");
 
 for (const key in tvmVariables) {
-    document.getElementById(key).addEventListener('input', (event) => {
+    document.getElementById(key).addEventListener("input", (event) => {
         tvmVariables[key].value = event.target.value;
         console.log(`${key}: ${tvmVariables[key]}`);
-    })
+    });
 }
 
 function calculateTVM() {
-
     tvmResult.textContent = "";
     let missingVariable = null;
 
@@ -64,12 +63,12 @@ function calculateTVM() {
                 if (tvmVariables.FV.value) {
                     const fv = Number(tvmVariables.FV.value);
                     const pvFromFV = fv * TVM.P.F(iy, n);
-                    pv += pvFromFV
+                    pv += pvFromFV;
                 }
                 if (tvmVariables.PMT.value) {
                     const pmt = Number(tvmVariables.PMT.value);
-                    const pvFromA = pmt * TVM.P.A(iy, n)
-                    pv += pvFromA
+                    const pvFromA = pmt * TVM.P.A(iy, n);
+                    pv += pvFromA;
                 }
                 tvmVariables.PV.value = String(pv);
 
@@ -118,7 +117,7 @@ function calculateTVM() {
 
             // }
             default: {
-                throw new Error(`WTF!? ${missingVariable}`)
+                throw new Error(`WTF!? ${missingVariable}`);
             }
         }
     } catch (error) {
@@ -139,6 +138,5 @@ function getInput(key) {
     }
 }
 
-const button = document.getElementById("tvm-calculate")
-button.onclick = () => calculateTVM() // setting on-click field in js instead of html
-
+const button = document.getElementById("tvm-calculate");
+button.onclick = () => calculateTVM(); // setting on-click field in js instead of html
--- test/utils.test.js
+++ test/utils.test.js
@@ -1,13 +1,13 @@
-import { describe, expect, test } from 'vitest';
-import { sumOfNaturalNumbers } from '../src/utils.js';
+import { describe, expect, test } from "vitest";
+import { sumOfNaturalNumbers } from "../src/utils.js";
 
-describe('Testing sumOfNaturalNumbers', () => {
+describe("Testing sumOfNaturalNumbers", () => {
     test.each([
         [0, 0],
         [1, 1],
         [2, 3],
         [10, 55],
-    ])('sumOfNaturalNumbers(%i) = %i', (n, sum) => {
-        expect(sumOfNaturalNumbers(n)).toBe(sum)
-    })
-})
+    ])("sumOfNaturalNumbers(%i) = %i", (n, sum) => {
+        expect(sumOfNaturalNumbers(n)).toBe(sum);
+    });
+});
