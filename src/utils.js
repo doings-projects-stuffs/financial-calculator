a4519f487d67d270f0896b81f4837f4b98e3971b
Zhanna Klimanova <zklimanova@yahoo.com>
2024-06-29 16:10:28 -0400

    Updates to derviative and newtons method

--- src/utils.js
+++ src/utils.js
@@ -66,14 +66,3 @@ export function newtown_raphson_method(x0, f, f_prime, tolerance, epsilon, max_i
 export function derivative(f, delta_x) {
     return (x) => (f(x + delta_x) - f(x)) / delta_x
 }
-
-/**
- * Compute the derivative of a tvm function using the definition of a derivative
- * @param {(i: number, n: number) => number} f
- * @param {number} n
- * @param {number} delta_x
- * @returns {(i: number) => number}
- */
-export function tvm_derivative(f, n, delta_x) {
-    return derivative((i) => f(i, n), delta_x)
-}
